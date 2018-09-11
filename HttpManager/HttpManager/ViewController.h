19b0a8c63f12387370bf4568bca79c7480605dd9
quanling <ios123@jeremy.local>
2018-09-11 15:40:23 +0800

    add something

---
+++ HttpManager/HttpManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpManager
+//
+//  Created by iOS123 on 2018/9/11.
+//  Copyright © 2018年 iOS123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HttpManager/HttpManager/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  HttpManager
+//
+//  Created by iOS123 on 2018/9/11.
+//  Copyright © 2018年 iOS123. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
